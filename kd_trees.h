95ccf8500d266b6457105b0b17194c82c30bb94c
sahusneha2004 <129661293+sahusneha2004@users.noreply.github.com>
2023-10-21 19:41:25 +0530

    Update kd_trees.h

--- kd_trees.h
+++ kd_trees.h
@@ -1,80 +1,101 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<malloc.h>
-#include<stdbool.h>
-
-#define k 2
-
-struct KDtree 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int k ;
+typedef struct Node
 {
-	int data[k];
-	struct KDtree *left;
-	struct KDtree *right;
-};
+    int* data ;
+    struct Node* left;
+    struct Node* right;
+} node;
 
-typedef struct KDtree node;
-
-node *insert_node(node *root , int point[] , int depth)
+node* insert_node(node* root, int* point, int depth)
 {
-	if (root == NULL)
+    node* newnode;
+    newnode = (node*)malloc(sizeof(node));
+	newnode->data = (int*)malloc(k * sizeof(int));
+    for (int i = 0; i < k; i++)
+    {
+        newnode->data[i] = point[i];
+    }
+    if (root == NULL)
+    {   root = newnode ;
+        newnode->left = NULL;
+        newnode->right = NULL;
+    }
+    else
+    {
+        int disc = depth % k;
+
+        if (point[disc] < root->data[disc])
+        {
+            root->left = insert_node(root->left, point, depth + 1);
+        }
+        else
+        {
+            root->right = insert_node(root->right, point, depth + 1);
+        }
+    }
+    return root;
+}
+int count= 0;
+void print_tree(node * tree ) 
+{ 	
+	if (tree)
 	{
-
-		node *Newnode;
-       		Newnode = (node*)malloc(sizeof(node));
-		for(int i = 0 ; i < k ; i++)
+		print_tree(tree->left);
+		for (int i = 0; i < k; i++)
 		{
-			Newnode->data[i] = point[i];
+			printf("%d ", tree->data[i]);
 		}
-		Newnode->left = NULL;
-		Newnode->right = NULL;
-	}	
-	else
-	{
-		int disc = depth%k;
-
-		if (point[disc] < (root->data[disc]))
-		{
-			root->left  = insert_node(root->left, point, depth + 1);
-		}
-
-    		else
-		{
-        		root->right = insert_node(root->right, point, depth + 1);
-		}
+		printf("\n");
+		print_tree(tree->right);
 	}
-		return root;
 }
-node *insert(node *root , int point[])
-{
-	return insert_node(root , point , 0);
-}
-
-void inorder(node *root, int depth) 
-{
-    if (root == NULL) return;
-
-    int cd = depth % k;
-    inorder(root->left, depth + 1);
-    printf("(%d , %d)", root->data[0] ,root->data[1]);
-    inorder(root->right, depth + 1);
-}
-
-
 
 int main()
 {
-	node *root = NULL;
-	int n;
-    	printf("Enter the number of points: ");
-    	scanf("%d", &n);
-    	int points[n][k];
+    node* root = NULL;
+    int n;
+    printf("Enter the number of points: ");
+    scanf("%d", &n);
+    printf("Enter the number of dimensions (k): ");
+    scanf("%d", &k);
 
-    	for (int i = 0; i < n; ++i)
-       	{
-        	scanf("%d %d", &points[i][0], &points[i][1]);
-        	root = insert(root, points[i]);
-    	}
-		inorder(root , 0);
+    // Allocate memory for the array of points
+    int** point = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++)
+    {
+        point[i] = (int*)malloc(k * sizeof(int));
+    }
 
-	return 0;
+    // Scan the values of points
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            scanf("%d", &point[i][j]);
+        }
+    }
+
+    // Insert the points in the tree
+    for (int i = 0; i < n; i++)
+    {
+        root = insert_node(root, point[i], 0);
+    }
+
+    // Don't forget to free the allocated memory for point arrays.
+	print_tree(root);
+	// print("%d",root->data[i]))
+	for(int i = 0; i < k; i++)
+	{
+		printf("%d",root->data[i]);
+	}
+    for (int i = 0; i < n; i++)
+    {
+        free(point[i]);
+    }
+    free(point);
+
+    return 0;
 }
